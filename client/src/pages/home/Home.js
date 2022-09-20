7b87be432d302956a3d1be5f3191e40fa706e76a
patriceandrea <andrea_pavico98@yahoo.ca>
2022-09-20 15:58:46 -0400

    adds featured and cart components

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Chart from '../../components/chart/Chart';
+import Featured from '../../components/featured/Featured';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Widget from '../../components/widget/Widgets';
@@ -15,6 +17,10 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className='charts'>
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
