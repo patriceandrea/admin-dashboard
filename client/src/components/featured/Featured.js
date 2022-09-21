3e70b29d76367410124cf88e5256ee08bb88cb37
patriceandrea <andrea_pavico98@yahoo.ca>
2022-09-21 16:20:44 -0400

    adds up arrow icon to the Featured component

--- client/src/components/featured/Featured.js
+++ client/src/components/featured/Featured.js
@@ -3,7 +3,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardDoubleArrowDownOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowDownOutlined';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 const Featured = () => {
   return (
@@ -24,22 +24,22 @@ const Featured = () => {
         <div className='summary'>
           <div className='item'>
             <div className='itemTitle'>Target</div>
-            <div className='itemResult'>
+            <div className='itemResult negative'>
               <KeyboardArrowDownIcon fontSize='small' />
               <div className='resultAmount'>$12.4k</div>
             </div>
           </div>
           <div className='item'>
-            <div className='itemTitle'>Target</div>
-            <div className='itemResult'>
-              <KeyboardArrowDownIcon fontSize='small' />
+            <div className='itemTitle'>Last Week</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpOutlinedIcon fontSize='small' />
               <div className='resultAmount'>$12.4k</div>
             </div>
           </div>
           <div className='item'>
-            <div className='itemTitle'>Target</div>
-            <div className='itemResult'>
-              <KeyboardArrowDownIcon fontSize='small' />
+            <div className='itemTitle'>Last Month</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpOutlinedIcon fontSize='small' />
               <div className='resultAmount'>$12.4k</div>
             </div>
           </div>
