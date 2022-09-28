3fdf814a9e566498605d4f49ba1f013ae8188891
patriceandrea <andrea_pavico98@yahoo.ca>
2022-09-28 18:34:27 -0400

    adds age column in datatable

--- client/src/components/datatable/Datatable.js
+++ client/src/components/datatable/Datatable.js
@@ -17,8 +17,14 @@ const columns = [
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    renderCell: (params) => {
+      return (
+        <>
+          <span>{params.row.lastName}</span>
+          <p>{params.row.age}</p>
+        </>
+      )
+    }
   },
 ];
 
