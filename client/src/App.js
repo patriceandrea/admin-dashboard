00109c904c3841fbee2a3ee1f87b7a08ad13f22e
patriceandrea <andrea_pavico98@yahoo.ca>
2022-09-29 13:33:11 -0400

    adds file and formSource file

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
+import { userInputs, productInputs } from "./formSource";
 
 
 function App() {
@@ -21,12 +22,12 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
             </Route>
           </Route>
         </Routes>
--- client/src/pages/new/New.js
+++ client/src/pages/new/New.js
@@ -2,56 +2,42 @@ import './new.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar'
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import { useState } from 'react';
 
-const New = () => {
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  console.log(file);
   return (
     <div className='new'>
       <Sidebar />
       <div className='newContainer'>
         <Navbar />
         <div className='top'>
-          <h1>Add new User</h1>
+          <h1>{title}</h1>
         </div>
         <div className='bottom'>
           <div className='left'>
-            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+            <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} alt="" />
           </div>
           <div className='right'>
             <form>
               <div className='formInput'>
-                <label>
+                <label htmlFor='file'>
                   Image: <DriveFolderUploadOutlinedIcon className='icon' />
                 </label>
-                <input type="file" id='file' style={{ display: "none" }} />
+                <input
+                  type="file"
+                  id='file'
+                  onChange={e => setFile(e.target.files[0])}
+                  style={{ display: "none" }} />
               </div>
-              <div className='formInput'>
-                <label>Username</label>
-                <input type="text" placeholder="john_doe" />
-              </div>
-              <div className='formInput'>
-                <label>Name and Surname</label>
-                <input type="text" placeholder="John Doe" />
-              </div>
-              <div className='formInput'>
-                <label>Email</label>
-                <input type="email" placeholder="john_doe@gmail.com" />
-              </div>
-              <div className='formInput'>
-                <label>Phone</label>
-                <input type="text" placeholder="+1 564 324 2445" />
-              </div>
-              <div className='formInput'>
-                <label>Password</label>
-                <input type="password" />
-              </div>
-              <div className='formInput'>
-                <label>Address</label>
-                <input type="text" placeholder="Elton St. 216 NewYork" />
-              </div>
-              <div className='formInput'>
-                <label>Country</label>
-                <input type="text" placeholder="USA" />
-              </div>
+              {inputs.map((input) => (
+                <div className='formInput' key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
               <button>Send</button>
             </form>
           </div>
