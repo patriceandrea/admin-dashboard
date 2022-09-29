e07dfbf97ca2e34d8db03577e49a8b5f0b72bcc9
patriceandrea <andrea_pavico98@yahoo.ca>
2022-09-28 23:28:16 -0400

    adds single components and adds css styling

--- client/src/pages/single/Single.js
+++ client/src/pages/single/Single.js
@@ -2,6 +2,7 @@ import './single.scss';
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import Chart from "../../components/chart/Chart"
+import Table from "../../components/table/Table"
 
 const Single = () => {
   return (
@@ -43,7 +44,10 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spending (Last 6 months)" />
           </div>
         </div>
-        <div className='bottom'></div>
+        <div className='bottom'>
+          <h1 className='title'>Last Transactions</h1>
+          <Table />
+        </div>
       </div>
     </div>
   )
